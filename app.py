dc729b9887eee96f4a8834685a3a8f0d78b5280d
Megusaka <logan36bassett@gmail.com>
2026-03-02 12:52:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,48 @@
-from flask import Flask, render_template
-import pymysql
+from flask import Flask, render_template, request, redirect, url_for
+import uuid
+import DatabaseConnection as db
 
 app = Flask(__name__)
 
-def get_db_connection():
-    print("Connecting to SQL")
-
-    connection = pymysql.connect(
-        host="localhost",       #"Megusaka.mysql.pythonanywhere-services.com" #http://127.0.0.1:5000/
-        user="",
-        password="",
-        database="",            #Megusaka$database_name
-        cursorclass=pymysql.cursors.DictCursor          #returns dictionary instead of just tuple
-                                                        #key = column name
-    )
-
-    print("Connected")
-    return connection
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    print("Route /")
-    try:
-        connection = get_db_connection()
-        with connection.cursor() as cursor:
-            print("Select Query Executing")
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        school = request.form.get("school")
 
-            cursor.execute("SELECT id, name, position FROM players")
-            rows = cursor.fetchall()
+        if first_name and last_name and school:
+            pii_id = str(uuid.uuid4())
 
-            print(f"Number of rows returned: {len(rows)}")
-            print("Rows:", rows)
-        connection.close()
-    except Exception as e:
-        print("Database error occurred:", e)
-        rows = []
-    return render_template("index.html", rows=rows)
+            db.insert_into_player_identifying_information(
+                pii_id,
+                first_name,
+                last_name,
+                school
+            )
+
+        return redirect(url_for("index")) #allows for url change
+
+
+    query = "SELECT pii_id, first_name, last_name, school FROM player_identifying_information ORDER BY first_name"
+    players = db.execute_read(query, ())
+
+    return render_template("index.html", players=players)
+
+
+
+@app.route("/player/<pii_id>")
+def player_detail(pii_id):
+    query = """
+        SELECT first_name, last_name, school
+        FROM player_identifying_information
+        WHERE pii_id = %s
+    """
+    player = db.execute_read(query, (pii_id,))
+
+    return render_template("player_detail.html", player=player)
+
 
 
 if __name__ == "__main__":
-    print("starting web server")
     app.run(debug=True)
